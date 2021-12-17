693a95e5f088434f99dc161470a0c3bde298178c
Aman Raj <amanpie9@gmail.com>
2021-12-17 19:07:54 +0530

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Something());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Something extends StatelessWidget {
+  const Something({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -34,7 +35,69 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hi"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                width: 190.0,
+                height: 190.0,
+                decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(
+                        fit: BoxFit.fill,
+                        image: new NetworkImage(
+                            "https://i.imgur.com/BoN9kdC.png")))),
+            Container(
+              height: 60,
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.all(15),
+              color: Colors.lightBlue,
+              // width: 40,
+              // height: 40,
+              child: Text(
+                'Girlscript',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              height: 40,
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              // width: 40,
+              // height: 40,
+              // color: Colors.green,
+              child: Text(
+                'girsclipt@gmail.com',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              height: 40,
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              // width: 40,
+              // height: 40,
+              // color: Colors.green,
+              child: Text(
+                '1111111111',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
